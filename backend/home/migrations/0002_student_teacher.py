60ae8815adc7e95387aea63f65ad3a5e2e22ef4d
Crowdbotics <team@crowdbotics.com>
2022-01-23 10:26:59 +0000

    Update models

--- backend/home/migrations/0002_student_teacher.py
+++ backend/home/migrations/0002_student_teacher.py
@@ -8,25 +8,41 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('home', '0001_load_initial_data'),
+        ("home", "0001_load_initial_data"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Student',
+            name="Student",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=256)),
-                ('email', models.EmailField(max_length=254)),
-                ('phone', models.DecimalField(decimal_places=10, max_digits=30)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=256)),
+                ("email", models.EmailField(max_length=254)),
+                ("phone", models.DecimalField(decimal_places=10, max_digits=30)),
             ],
         ),
         migrations.CreateModel(
-            name='Teacher',
+            name="Teacher",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=256)),
-                ('email', models.EmailField(max_length=254)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=256)),
+                ("email", models.EmailField(max_length=254)),
             ],
         ),
     ]
--- backend/home/views.py
+++ backend/home/views.py
@@ -1,3 +1,5 @@
+from .models import Student, Teacher
+from django.views.generic import CreateView, UpdateView, DetailView, ListView
 from django.shortcuts import render
 
 
